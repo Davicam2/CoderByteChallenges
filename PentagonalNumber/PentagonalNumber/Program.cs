9461aa65ab7398df5757ab515f96326e779fcaaf
Cameron Davis <cameronwdavis88@gmail.com>
2018-07-25 19:15:06 +0300

    GitHub Readability on .exe of programs

--- PentagonalNumber/PentagonalNumber/Program.cs
+++ PentagonalNumber/PentagonalNumber/Program.cs
@@ -8,8 +8,17 @@ namespace PentagonalNumber
 {
     class Program
     {
-        static void Main(string[] args){
-            Console.WriteLine("number of dots = " + PentNumber(int.Parse(Console.ReadLine())));
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-This Problem is called Pentagonal Numbers.\r\n");
+
+            Console.WriteLine("-The Problem is meant to simulate a figure of dots that starts with one and grows outward  \r\n" +
+                              "in the form of 5 additional dots each layer that is added. You input the nuber of layers of dots \r\n" +
+                              "and the program will tell you the total amount of dots that exist once that layer is added.\r\n");
+            Console.Write("Please select the number of layers in the pentagonal structure:");
+            int.TryParse(Console.ReadLine(), out int layers);
+
+            Console.WriteLine("\r\nTotal Number of Dots = " +  PentNumber(layers));
             Console.ReadKey();            
         }
         static int PentNumber(int num){
--- ScaleBalancing/ScaleBalancing/ScaleBalancing/Program.cs
+++ ScaleBalancing/ScaleBalancing/ScaleBalancing/Program.cs
@@ -12,17 +12,28 @@ namespace ScaleBalancing
 
         static void Main(string[] args)
         {
+
+
+
             string scales = "[13, 4]";
             string weights = "[1, 2, 3, 6, 14]";
             int[] Aweights = { 1, 2, 3, 10, 14 };
             int[] Ascales = { 13, 4 };
 
-            Console.WriteLine(BalanceScales(Ascales, Aweights));
+            Console.WriteLine("-This problem is called ScaleBalancing.\r\n");
+
+            Console.WriteLine("In this problem you are given a scale with weights on either side, the goal is to find a combination\r\n" +
+                              "of up to two of the given weights with which to balance the scales.");
+            Console.WriteLine(string.Format("-The scale weights are: {0}-{1} \r\n" +
+                                            "-The Weights available are: {2}-{3}-{4}-{5}-{6}\r\n"
+                              ,Ascales[0],Ascales[1],Aweights[0],Aweights[1],Aweights[2],Aweights[3],Aweights[4]));
+
+            Console.WriteLine("Weights Required: " + BalanceScales(Ascales, Aweights));
             Console.ReadKey();
 
         }
 
-        static string BalanceScales(int[] scales, int[] weights)
+        static string BalanceScales(int[] scales, int[] weights) 
         {
             int difference = Math.Abs(scales[0] - scales[1]);
 

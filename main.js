1af8e6df61e60509bb9a3d7a84a5d9fae956f0ca
Davicam2 <Cameronwdavis88@gmail.com>
2019-05-31 08:45:08 -0600

    cleaning code

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-﻿
-window.onload = function () {
-    
-    var output = document.getElementById("txtOut");
-    document.getElementById("btnIn").onclick = function (evt) {
-        var marbles = document.getElementById("txtIn").value;
-        document.getElementById("txtOut").value = calculateDays(marbles, txtOut);
-        
-    }
-}
 
 function calculateDays(marbles) {
     let vCount = 0, hrCount = 0, cycles = 0;
@@ -22,10 +12,8 @@ function calculateDays(marbles) {
     let cycledQueue = populateArray();
     let refQueue = populateArray();
     let tempQueue = populateArray();
-
-
+    
     while (!cycled) {
-
         //handles shifting 5 balls into the minute queue and tipping into the Vmin queue.
         if (vCount < 12) {
             shiftElements(mins, cycledQueue);
@@ -33,7 +21,6 @@ function calculateDays(marbles) {
             vmins.unshift(mins.shift());
             dumpMarbles(cycledQueue, mins, 4);
             vCount++;
-
         }
         //handles shifting balls into the hours queue and dumping the Vmin queue.
         else {
@@ -41,7 +28,6 @@ function calculateDays(marbles) {
             dumpMarbles(cycledQueue, vmins, 11);
             hrCount++;
             vCount = 0;
-
         }
         //dumps the hours queue and breaks the while loop.
         if (hrCount == 12) {
@@ -52,13 +38,7 @@ function calculateDays(marbles) {
             cycles = 1;
             cycled = true;
         }
-
-        //if (cycledQueue.toString() == refQueue.toString()) {
-            //cycled = true;
-            //return cycles / 2;
-        //}
     }
-
     //establish the pattern of movement across one cycle of the machine
     for (var i = 0; i < marbles; i++) {
         for (var j = 0; j < marbles; j++) {
@@ -67,7 +47,6 @@ function calculateDays(marbles) {
             }
         }
     }
-
     //simulates 12 hour movement using the pattern map
     while (cycledQueue.toString() !== refQueue.toString()) {
         for (var i = 0; i < marbles; i++) {
@@ -76,27 +55,24 @@ function calculateDays(marbles) {
         cycledQueue = tempQueue.slice(0);
         cycles++;
     }
-
+    
+    //////////////////////////
     //returns the answer
     return cycles / 2;
-
-
-
-
+    /////////////////////////
+    
     //fills the min queue with elements from the base queue
     function shiftElements(arrGaining, arrLosing) {
         for (var i = 0; i < 5; i++) {
             arrGaining.unshift(arrLosing.shift());
         }
     }
-
     //add back in reverse order from time queue to base queue
     function dumpMarbles(arrGaining, arrLosing, number) {
         for (var i = 0; i < number; i++) {
             arrGaining.push(arrLosing.shift());
         }
     }
-
     //util function
     function populateArray() {
         let arr = [];
@@ -105,7 +81,6 @@ function calculateDays(marbles) {
         }
         return arr;
     }
-
 }
 
 
